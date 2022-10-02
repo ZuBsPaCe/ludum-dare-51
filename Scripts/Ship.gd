fadf393c154f0024277a5aace49156ad038d9c7c
ZuBsPaCe <kurt.rudin@gmx.net>
2022-10-02 13:12:15 +0200

    Nice ship sprite and turn animation

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 
 onready var _collision := $CollisionPolygon2D
+onready var _window := $Window
 
 
 var collision_enabled: bool setget _set_collision_enabled, _get_collision_enabled
@@ -13,9 +14,30 @@ var _trail_countdown := _init_trail_countdown
 
 var _last_position := Vector2.ZERO
 
+var _window_offset := 0.0
+var _target_window_offset := 0.0
+var _window_speed := 4.0 * 3.0
+
 
 func _process(delta):
+	var last_rotation := rotation
+
 	rotation = Globals.get_global_mouse_position().angle_to_point(position)
+	
+	var rotation_speed: float = (rotation - last_rotation) / delta
+	if rotation_speed > 0.0 and rotation_speed > PI * 0.25:
+		_target_window_offset = 4.0 * clamp(rotation_speed / (PI * 0.25), 0.0, 1.0)
+	elif rotation_speed < 0.0 and rotation_speed < -PI * 0.25:
+		_target_window_offset = 4.0 * clamp(rotation_speed / (PI * 0.25), -1.0, 0.0)
+	else:
+		_target_window_offset = 0.0
+	
+	if _window_offset < _target_window_offset:
+		_window_offset = min(_window_offset + _window_speed * delta, _target_window_offset)
+	elif _window_offset > _target_window_offset:
+		_window_offset = max(_window_offset - _window_speed * delta, _target_window_offset)
+		
+	_window.position.y = _window_offset
 	
 	var ship_velocity := position - _last_position
 	var ship_velocity_dir := ship_velocity.normalized()
